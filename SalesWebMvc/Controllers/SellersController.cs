6d388ddbd7b648e334ca57cbf020c8e9ed4a12c7
sktbang <igordsmolina@gmail.com>
2019-11-06 15:17:40 -0300

    Adicionando details no seller e eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -49,6 +49,21 @@ namespace SalesWebMvc.Controllers
 
         }
 
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null){
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller )
